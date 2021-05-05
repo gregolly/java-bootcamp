322660dc09e1b87ee166401467c813b33d2af852
Gregolly frança <42480355+gregolly@users.noreply.github.com>
2021-05-05 18:58:17 -0400

    Solução da tarefa menu

--- Menu.java
+++ Menu.java
@@ -2,32 +2,38 @@ import java.util.Scanner;
 
 public class Menu {
   public static void main(String[] args) {
-    System.out.println("| Menu             |");
-    System.out.println("|   Opções:             |");
-    System.out.println("|         1. Opção 1       |");
-    System.out.println("|         2. Opção 2       |");
-    System.out.println("|         3. Sair       |");
-    Scanner menu = new Scanner(System.in);
 
-    System.out.println("Selecione uma opção: ");
-    int opcao = menu.nextInt();
-
-    int numero = 3;
+    int opcao = 0;
     do {
+      System.out.println("|        Menu           |");
+      System.out.println("|       Opções :        |");
+      System.out.println("|      1. Opção 1       |");
+      System.out.println("|      2. Opção 2       |");
+      System.out.println("|        Sair           |");
+      Scanner menu = new Scanner(System.in);
+
+      System.out.println("| Selecione uma Opção:  |");
+      opcao = menu.nextInt();
+
       switch (opcao) {
+
         case 1:
-          System.out.print("Opção 1 selecionada");
+          System.out.println("Opcao 1 Selecionada");
           break;
+
         case 2:
-          System.out.print("Opção 2 selecionada");
+          System.out.println("Opcao 2 Selecionada");
           break;
+
         case 3:
-          System.out.print("Sair");
+          System.out.println("Sair");
           break;
+
         default:
-          System.out.println("Seleção inválida");
+
+          System.out.println("Opcao Inválida");
           break;
       }
-    } while (opcao != numero);
+    } while (opcao < 3);
   }
 }
